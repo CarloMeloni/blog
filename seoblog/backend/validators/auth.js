538f943c8c5824287bcb5da27f00e2917fabc248
carloMeloni <carlomeloni63@gmail.com>
2020-11-21 10:45:40 +0100

    Signup Validators

---
+++ seoblog/backend/validators/auth.js
@@ -0,0 +1,14 @@
+const {check} = require('express-validator');
+
+exports.userSignupValidator = [
+    check('name')
+        .not()
+        .isEmpty()
+        .withMessage('Nome richiesto.'),
+    check('email')
+        .isEmail()
+        .withMessage('Deve essere un indirizzo e-mail valido.'),
+    check('password')
+        .isLength({ min: 6 })
+        .withMessage('La password deve avere almeno 6 caratteri.'),
+];
