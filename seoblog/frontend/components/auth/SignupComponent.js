1854d93589cf58448a49f38e727a21a55d8ce11f
carloMeloni <carlomeloni63@gmail.com>
2020-11-24 17:03:49 +0100

    Signup Action and Alerts

--- seoblog/frontend/components/auth/SignupComponent.js
+++ seoblog/frontend/components/auth/SignupComponent.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import { signup } from '../../actions/auth';
 
 const SignupComponent = () => {
     const [values, setValues] = useState({
@@ -21,34 +22,40 @@ const SignupComponent = () => {
         showForm
     } = values;
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setValues({ ...values, loading: true, error: false });
+        const user = {name, email, password};
+
+        signup(user)
+            .then(data => {
+                if(data.error) {
+                    setValues({ ...values, error: data.error })
+                } else {
+                    setValues({ ...values, name: '', email: '', password: '', error: '', loading: false, message: data.message, showForm: false })
+                }
+            }) 
+    }
+    
     const handleChange = name => (e) => {
         setValues({ ...values, error: false, [name]: e.target.value });
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.table({
-            name,
-            email,
-            password,
-            error,
-            loading,
-            message,
-            showForm
-        })
-    }
+    const showLoading = () => (loading ? <div className="alert alert-info">Caricamento</div> : '');
+    const showError = () => (error ? <div className="alert alert-danger">{error}</div> : '');
+    const showMessage = () => (message ? <div className="alert alert-info">{message}</div> : '');
 
     const signupForm = () => {
         return (
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
-                    <input value={name} onChange={handleChange('name')} type="text" className="form-group" placeholder="Il tuo nome" />
+                    <input value={name} onChange={handleChange('name')} type="text" className="form-control" placeholder="Il tuo nome" />
                 </div>
                 <div className="form-group">
-                    <input value={email} onChange={handleChange('email')} type="email" className="form-group" placeholder="La tua email" />
+                    <input value={email} onChange={handleChange('email')} type="email" className="form-control" placeholder="La tua email" />
                 </div>
                 <div className="form-group">
-                    <input value={password} onChange={handleChange('password')} type="password" className="form-group" placeholder="Inserisci la password" />
+                    <input value={password} onChange={handleChange('password')} type="password" className="form-control" placeholder="Inserisci la password" />
                 </div>
                 <div>
                     <button className="btn btn-primary">Registrati</button>
@@ -59,7 +66,10 @@ const SignupComponent = () => {
 
     return (
         <React.Fragment>
-            {signupForm()}
+            {showError()}
+            {showMessage()}
+            {showLoading()}
+            {showForm && signupForm()}
         </React.Fragment>
         
     )
--- seoblog/frontend/pages/signup.js
+++ seoblog/frontend/pages/signup.js
@@ -5,8 +5,12 @@ import Link from 'next/link';
 const Signup = () => {
     return (
         <Layout>
-        <h2>Signup PAGE!</h2>
-        <SignupComponent />
+        <h2 className="text-center">REGISTRATI</h2>
+        <div className="row">
+            <div className="col-md-6 offset-md-3">
+                <SignupComponent />
+            </div> 
+        </div>
     </Layout>
     )
 
