d3b7a8e86b63c7af654ef5bed5639b54f20d0e84
carloMeloni <carlomeloni63@gmail.com>
2020-12-01 14:52:03 +0100

    Creating Category Page (frontend)

---
+++ seoblog/frontend/pages/admin/crud/category-tag.js
@@ -0,0 +1,28 @@
+import Layout from '../../../components/Layout';
+import Admin from '../../../components/auth/Admin';
+import Link from 'next/link';
+
+const CategoryTag = () => {
+    return (
+        <Layout>
+            <Admin>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12 pt-5 pb-5">
+                            <h2>Gestisci le categorie e i tags</h2>
+                        </div>
+                        <div className="col-md-6">
+                            <p>Categorie</p>
+                        </div>
+                        <div className="col-md-6">
+                            <p>Tag</p>
+                        </div>
+                    </div>
+                </div>
+            </Admin>
+    </Layout>
+    )
+
+}
+
+export default CategoryTag;
--- seoblog/frontend/pages/admin/index.js
+++ seoblog/frontend/pages/admin/index.js
@@ -6,7 +6,25 @@ const AdminIndex = () => {
     return (
         <Layout>
             <Admin>
-                <h2>AdminINDEX PAGE!</h2>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12 pt-5 pb-5">
+                            <h2>ADMIN DASHBOARD</h2>
+                        </div>
+                        <div className="col-md-4">
+                        <ul className="list-group">
+                            <li className="list-group-item">
+                                <Link href="/admin/crud/category-tag">
+                                    <a>Create Category</a>
+                                </Link>
+                            </li>
+                        </ul>
+                        </div>
+                        <div className="col-md-8">
+                            RIGHT
+                        </div>
+                    </div>
+                </div>
             </Admin>
     </Layout>
     )
