9b2c01a7d2fc984ae483e5b4221afb7d12cefe6a
carloMeloni <carlomeloni63@gmail.com>
2020-12-03 17:55:04 +0100

    Creating Blog with Photo

--- seoblog/backend/controllers/blog.js
+++ seoblog/backend/controllers/blog.js
@@ -1,3 +1,51 @@
-exports.time = (req, res) => {
-    res.json({ time: Date().toString() })
+const Blog = require('../models/blog');
+const Category = require('../models/category');
+const Tag = require('../models/tag');
+const formidable = require('formidable');
+const slugify = require('slugify');
+const stripHtml = require('string-strip-html');
+const _ = require('lodash');
+const { errorHandler } = require('../helpers/dbErrorHandler');
+const fs = require('fs');
+
+exports.create = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: "L'immagine non puo' essere caricata."
+            });
+        }
+        const { title, body, categories, tags } = fields;
+
+        let blog = new Blog();
+        blog.title = title;
+        blog.body = body;
+        blog.slug = slugify(title).toLowerCase();
+        blog.categories = categories;
+        blog.tags = tags;
+        blog.mtitle = `${title} | ${process.env.APP_NAME}`;
+        blog.mdescription = stripHtml(body.substring(0, 160));
+        blog.postedBy = req.user._id;
+
+        if(files.photo) {
+            if(files.photo.size > 10000000) {
+                return res.status(400).json({
+                    error: "Le dimensioni dell'immagine devono essere inferiori a 1mb."
+                });
+            }
+            blog.photo.data = fs.readFileSync(files.photo.path);
+            blog.photo.contentType = files.photo.type;
+        }
+
+        blog.save((err, result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        })
+    })
 }
