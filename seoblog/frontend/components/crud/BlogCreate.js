a86bcdeccec7ced0e4e986618cdc58112c460c89
carloMeloni <carlomeloni63@gmail.com>
2020-12-09 12:08:56 +0100

    React quill form, event handlers and localStorage

--- seoblog/frontend/components/crud/BlogCreate.js
+++ seoblog/frontend/components/crud/BlogCreate.js
@@ -6,12 +6,24 @@ import { withRouter } from 'next/router';
 import { getCookie, isAuth } from '../../actions/auth';
 import { getCategories } from '../../actions/category';
 import { getTags } from '../../actions/tag';
-import { CreateBlog } from '../../actions/blog';
+import { createBlog } from '../../actions/blog';
 const ReactQuill = dynamic(() => import('react-quill'), {ssr: false});
 import '../../node_modules/react-quill/dist/quill.snow.css';
 
 const BlogCreate = ({ router }) => {
-    const [body, setBody] = useState({});
+    const blogFromLs = () => {
+        if(typeof window === 'undefined') {
+            return false;
+        }
+
+        if(localStorage.getItem('blog')) {
+            return JSON.parse(localStorage.getItem('blog'));
+        } else {
+            return false;
+        }
+    };
+
+    const [body, setBody] = useState(blogFromLs());
     const [values, setValues] = useState({
         error: '',
         sizeError: '',
@@ -22,6 +34,10 @@ const BlogCreate = ({ router }) => {
     });
 
     const { error, sizeError, success, formData, title, hidePublishButton } = values;
+
+    useEffect(() => {
+        setValues({ ...values, formData: new FormData(),  })
+    }, [router])
     
     const publishBlog = (e) => {
         e.preventDefault();
@@ -29,11 +45,19 @@ const BlogCreate = ({ router }) => {
     };
 
     const handleChange = name => e => {
-        console.log(e.target.value);
+        //console.log(e.target.value);
+        const value = name === 'photo' ? e.target.files[0] : e.target.value;
+        formData.set(name, value);
+        setValues({ ...values, [name]: value, formData, error: '' })
     }
 
     const handleBody = e => {
-        console.log(e);
+        //console.log(e);
+        setBody(e);
+        formData.set('body', e);
+        if(typeof window !== 'undefined') {
+            localStorage.setItem('blog', JSON.stringify(e));
+        }
     }
 
     const createBlogForm = () => {
@@ -45,7 +69,12 @@ const BlogCreate = ({ router }) => {
                 </div>
 
                 <div className="form-group">
-                    <ReactQuill value={body} placeholder="Scrivi qualcosa di brutto..." onChange={handleBody} />
+                    <ReactQuill 
+                        modules={CreateBlog.modules} 
+                        formats={CreateBlog.formats} 
+                        value={body} 
+                        placeholder="Scrivi qualcosa di brutto..." 
+                        onChange={handleBody} />
                 </div>
 
                 <div>
@@ -59,8 +88,41 @@ const BlogCreate = ({ router }) => {
     return (
         <div>
             {createBlogForm()}
+            <hr/>
+            {JSON.stringify(title)}
+            <hr/>
+            {JSON.stringify(body)}
         </div>
     )
 }
+let CreateBlog = {};
+CreateBlog.modules = {
+    toolbar: [
+        [{ header: '1' }, { header: '2' }, { header: [3, 4, 5, 6] }, { font: [] }],
+        [{ size: [] }],
+        ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        ['link', 'image', 'video'],
+        ['clean'],
+        ['code-block']
+    ]
+};
+ 
+CreateBlog.formats = [
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'link',
+    'image',
+    'video',
+    'code-block'
+];
 
 export default withRouter(BlogCreate);
