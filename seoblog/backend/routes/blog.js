c47b292903a1a0fa946ac8fe9c9e8bc40d5de4fa
carloMeloni <carlomeloni63@gmail.com>
2021-02-07 11:28:20 +0100

    Deleting Blogs by Admin Page

--- seoblog/backend/routes/blog.js
+++ seoblog/backend/routes/blog.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const { requireSignin, adminMiddleware } = require('../controllers/auth');
+const { requireSignin, authMiddleware } = require('../controllers/auth');
 const {create, list, listAllBlogCategoriesTags, read, remove, update, photo, listRelated} = require('../controllers/blog')
 
-router.post('/blog', requireSignin, adminMiddleware, create)
+router.post('/blog', requireSignin, authMiddleware, create)
 router.get('/blogs', list)
 router.post('/blogs-categories-tags', listAllBlogCategoriesTags)
 router.get('/blog/:slug', read)
-router.delete('/blog/:slug', requireSignin, adminMiddleware, remove)
-router.put('/blog/:slug', requireSignin, adminMiddleware, update)
+router.delete('/blog/:slug', requireSignin, authMiddleware, remove)
+router.put('/blog/:slug', requireSignin, authMiddleware, update)
 router.get('/blog/photo/:slug', photo)
 router.post('/blogs/related', listRelated)
 
--- seoblog/frontend/components/crud/ReadBlogs.js
+++ seoblog/frontend/components/crud/ReadBlogs.js
@@ -2,11 +2,64 @@ import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { getCookie, isAuth } from '../../actions/auth';
 import { list, removeBlog } from '../../actions/blog';
+import moment from 'moment';
 
 const ReadBlogs = () => {
+    const [blogs, setBlogs] = useState([]);
+    const [message, setMessage] = useState('');
+    const token = getCookie('token');
+
+    useEffect(() => {
+        loadBlogs();
+    }, []);
+
+    const loadBlogs = () => {
+        list().then(data => {
+            if(data.error) {
+                console.log(data.error);
+            } else {
+                setBlogs(data);
+            }
+        });
+    };
+
+    const deleteBlog = (slug) => {
+        removeBlog(slug, token)
+            .then(data => {
+                if(data.error) {
+                    console.log(data.error);
+                } else {
+                    setMessage('L\'articolo e\' stato rimosso');
+                    loadBlogs();
+                }
+            }) 
+    };
+
+    const deleteConfirm = (slug) => {
+        let answer = window.confirm('Sei sicuro di voler eliminare l\'articolo?');
+        if(answer) {
+            deleteBlog(slug);
+        };
+    };
+
+    const showAllBlogs = () => {
+        return blogs.map((blog, idx) => (
+            <div key={idx} className="mt-5">
+                <h3>{blog.title}</h3>
+                <p className="mark">Scritto da {blog.postedBy.name} | Pubblicato il {moment(blog.updatedAt).format('DD/MM/YYYY')} alle {moment(blog.updatedAt).format('hh:mm:ss')}</p>
+                <button className="btn btn-sm btn-danger" onClick={() => deleteConfirm(blog.slug)}>Elimina</button>
+            </div>
+        ));
+    }
+
     return (
         <>
-            <p>Aggiorna o rimuovi un articolo</p>
+            <div className="row">
+                <div className="col-md-12">
+                    {message && <div className="alert alert-warning">{message}</div>}
+                    {showAllBlogs()}
+                </div>
+            </div>
         </>
     )
 };
