c31fc62f1d0491bf889e37a124fae84a21c7a4a5
carloMeloni <carlomeloni63@gmail.com>
2021-07-10 18:17:02 +0200

    showing checked categories and tags

--- seoblog/frontend/components/crud/BlogUpdate.js
+++ seoblog/frontend/components/crud/BlogUpdate.js
@@ -7,11 +7,17 @@ import { getCookie, isAuth } from '../../actions/auth';
 import { getCategories } from '../../actions/category';
 import { getTags } from '../../actions/tag';
 import { singleBlog, updateBlog } from '../../actions/blog';
-const ReactQuill = dynamic(() => import('react-quill'), {ssr: false});
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 import '../../node_modules/react-quill/dist/quill.snow.css';
 
-const BlogUpdate = ({router}) => {
+const BlogUpdate = ({ router }) => {
     const [body, setBody] = useState('');
+
+    const [categories, setCategories] = useState([]);
+    const [tags, setTags] = useState([]);
+    const [checkedCategories, setCheckedCategories] = useState([]);
+    const [checkedTags, setCheckedTags] = useState([]);
+
     const [values, setValues] = useState({
         error: '',
         success: '',
@@ -28,22 +34,143 @@ const BlogUpdate = ({router}) => {
 
 
     useEffect(() => {
-        setValues({ ...values, formData: new FormData(),  });
+        setValues({ ...values, formData: new FormData(), });
         initBlog();
+        initCategories();
+        initTags();
     }, [router]);
 
     const initBlog = () => {
-        if(router.query.slug) {
+        if (router.query.slug) {
             singleBlog(router.query.slug)
                 .then(data => {
-                    if(data.error) {
+                    if (data.error) {
                         console.log(data.error);
                     } else {
-                        setValues({ ...values, title: data.title});
+                        console.log('data', data)
+                        setValues({ ...values, title: data.title });
                         setBody(data.body)
+                        setCategoriesArray(data.categories)
+                        setTagsArray(data.tags)
                     }
                 });
         }
+    };
+
+    const setCategoriesArray = (blogCategories) => {
+        let cArr = [];
+        blogCategories.map((cat, i) => {
+            cArr.push(cat._id)
+        })
+        console.log('cArr', cArr)
+
+        setCheckedCategories(cArr);
+        console.log('checkedCategories', checkedCategories)
+    }
+
+    const setTagsArray = (blogTags) => {
+        let tArr = [];
+        blogTags.map((tag, i) => {
+            tArr.push(tag._id)
+        })
+        console.log('tArr', tArr)
+
+        setCheckedTags(tArr);
+        console.log('checkedTags', checkedTags)
+    }
+
+    const initCategories = () => {
+        getCategories()
+            .then(data => {
+                if (data.err) {
+                    setValues({ ...values, error: data.err })
+                } else {
+                    setCategories(data);
+                }
+            })
+    }
+
+    const initTags = () => {
+        getTags()
+            .then(data => {
+                if (data.err) {
+                    setValues({ ...values, error: data.err });
+                } else {
+                    setTags(data);
+                }
+            })
+    }
+
+    const handleToggle = (cat) => () => {
+        setValues({ ...values, error: '' });
+        const clickedCategory = checkedCategories.indexOf(cat);
+        const all = [...checkedCategories];
+        if (clickedCategory === -1) {
+            all.push(cat);
+        } else {
+            all.splice(clickedCategory, 1);
+        }
+
+        console.log(all);
+        setCheckedCategories(all);
+        formData.set('categories', all);
+    }
+
+    const handleToggleTag = (tag) => () => {
+        setValues({ ...values, error: '' });
+        const clickedTag = checkedTags.indexOf(tag);
+        const all = [...checkedTags];
+        if (clickedTag === -1) {
+            all.push(tag);
+        } else {
+            all.splice(clickedTag, 1);
+        }
+
+        console.log(all);
+        setCheckedTags(all);
+        formData.set('tags', all);
+    }
+
+    const findOutCategory = (cat) => {
+        const result = checkedCategories.indexOf(cat._id);
+        console.log('result', result)
+        if (result !== -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const findOutTag = (tag) => {
+        const result = checkedTags.indexOf(tag._id);
+        console.log('result', result)
+        if (result !== -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const showCategories = () => {
+        return (
+            categories && categories.map((cat, i) => (
+                <li key={i} className="list-unstyled">
+                    <input onChange={handleToggle(cat._id)} checked={findOutCategory(cat)} type="checkbox" className="mr-2" />
+                    <label className="form-check-label">{cat.name}</label>
+                </li>
+            ))
+        )
+    };
+
+    const showTags = () => {
+        return (
+            tags && tags.map((tag, i) => (
+                <li key={i} className="list-unstyled">
+                    <input onChange={handleToggleTag(tag._id)} checked={findOutTag(tag)} type="checkbox" className="mr-2" />
+                    <label className="form-check-label">{tag.name}</label>
+                </li>
+            ))
+        )
     };
 
     const handleBody = e => {
@@ -71,12 +198,12 @@ const BlogUpdate = ({router}) => {
                 </div>
 
                 <div className="form-group">
-                    <ReactQuill 
-                        style={{backgroundColor: "#fff"}}
-                        modules={updateBlogg.modules} 
-                        formats={updateBlogg.formats} 
-                        value={body} 
-                        placeholder="Scrivi qualcosa..." 
+                    <ReactQuill
+                        style={{ backgroundColor: "#fff" }}
+                        modules={updateBlogg.modules}
+                        formats={updateBlogg.formats}
+                        value={body}
+                        placeholder="Scrivi qualcosa..."
                         onChange={handleBody} />
                 </div>
 
@@ -91,15 +218,31 @@ const BlogUpdate = ({router}) => {
         <div className="container-fluid pb-5">
             <div className="row">
                 <div className="col-8">
+                    {updateBlogForm()}
                     <div className="pt-3">
-                        {updateBlogForm()}
+                        <p>Success or error message</p>
                     </div>
                 </div>
-                <div className="col-4">
+                <div className="col-md-4">
                     <div>
                         <div className="form-group pb-2">
-                        <h5>Immagine Articolo</h5>
+                            <h5>Immagine Articolo</h5>
+                            <hr />
+                            <small className="text-muted">Dimensioni max: 1mb</small>
+                            <label className="btn btn-outline-danger m-3">Carica Immagine
+                                <input onChange={handleChange('photo')} type="file" accept="image/*" hidden />
+                            </label>
                         </div>
+                    </div>
+                    <div>
+                        <h5>Categorie</h5>
+                        <hr />
+                        <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>{showCategories()}</ul>
+                    </div>
+                    <div>
+                        <h5>Tag</h5>
+                        <hr />
+                        <ul style={{ maxHeight: "200px", overflowY: "scroll" }}>{showTags()}</ul>
                     </div>
                 </div>
             </div>
@@ -119,7 +262,7 @@ updateBlogg.modules = {
         ['code-block']
     ]
 };
- 
+
 updateBlogg.formats = [
     'header',
     'font',
