605dc803cb12746b2dd8b31c914a90638648d2de
carloMeloni <carlomeloni63@gmail.com>
2021-11-08 12:28:57 +0100

    small backend fix

--- seoblog/backend/routes/blog.js
+++ seoblog/backend/routes/blog.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { requireSignin, adminMiddleware, authMiddleware } = require('../controllers/auth');
+const { requireSignin, adminMiddleware, authMiddleware, canUpdateDeleteBlogs } = require('../controllers/auth');
 const {create,
      list,
      listAllBlogCategoriesTags,
@@ -26,7 +26,7 @@ router.get('/blogs/search', listSearch)
 //AUTH USER BLOG CRUD
 router.post('/user/blog', requireSignin, authMiddleware, create)
 router.get('/:username/blogs', listByUser)
-router.delete('/user/blog/:slug', requireSignin, authMiddleware, remove)
-router.put('/user/blog/:slug', requireSignin, authMiddleware, update)
+router.delete('/user/blog/:slug', requireSignin, authMiddleware, canUpdateDeleteBlogs, remove)
+router.put('/user/blog/:slug', requireSignin, authMiddleware, canUpdateDeleteBlogs, update)
 
 module.exports = router;
